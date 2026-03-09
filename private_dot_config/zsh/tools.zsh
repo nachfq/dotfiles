16e13ab59feab777152644267778cfd2dd250b84
Ignacio <ignacio@cobuilders.xyz>
2026-03-09 09:39:36 -0300

    Added non tracked private tools

--- private_dot_config/zsh/tools.zsh
+++ private_dot_config/zsh/tools.zsh
@@ -1,9 +1,4 @@
 # fzf
 [ -f "$HOME/.fzf.zsh" ] && source "$HOME/.fzf.zsh"
-
-# nvm (interactive)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" # Extra completions (optional)
-
+# zsh
 [ -f "/home/$USER/.zsh/completion/_zkstack.zsh" ] && source "/home/$USER/.zsh/completion/_zkstack.zsh"
-[ -f "/home/$USER/.openclaw/completions/openclaw.zsh" ] && source "/home/$USER/.openclaw/completions/openclaw.zsh"
